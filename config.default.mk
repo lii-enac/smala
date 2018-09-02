5f50edda1258d9c1f73f319cfa82ddbc5b1cc084
stephane conversy <conversy@djnn.net>
2018-09-02 22:21:24 +0200

    change default config filename so that the extension is .mk

---
+++ config.default.mk
@@ -0,0 +1,11 @@
+build_dir := build
+djnn_include_path_c := ../local-install/include
+djnn_lib_path_c := ../local-install/lib
+djnn_include_path_cpp := ../djnn-cpp/src
+djnn_lib_path_cpp := ../djnn-cpp/build
+
+cookbook_app_for_make_test := button
+
+# cross-compile support
+#cross_prefix := em
+

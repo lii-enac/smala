6f247e2c8ee7f4152593951bbf742f6826c84f23
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-01-20 17:24:03 +0100

    cpp_builder - added a guard and error message on addChildrenTo

--- src/cpp_builder.cpp
+++ src/cpp_builder.cpp
@@ -1269,6 +1269,9 @@ namespace Smala
       m_parent_list.push_back (new BuildNode (new_name, m_parent_list.back ()));
       /* FIXME dirty trick to set the parent name of the enclosed nodes*/
       node->set_build_name (new_name);
+      os << "if (" << new_name << " == nullptr)\n";
+      indent (os);
+      os << " cerr <<  endl << endl << \"ERROR - processing addChildrenTo - the component \\\"\" << \"" << node->name () << "\\\"\" << \" is null or do not exist yet\" << endl << endl;\n";
     } else {
       m_parent_list.push_back (new BuildNode (s.first, m_parent_list.back ()));
       /* FIXME dirty trick to set the parent name of the enclosed nodes*/

b50ba1cef5f76f43d22c63023c95313eceff9373
stephane conversy <conversy@gmail.com>
2020-07-16 21:26:33 +0200

    add crazyflie cookbook

---
+++ cookbook/extra/crazyflie_drone_app/cookbook_app.mk
@@ -0,0 +1,60 @@
+# usage :
+#
+# add this to config.mk in smala/:
+# cookbook_apps_extra := extra/crazyflie_drone_app
+# cookbook_app_for_make_test := crazyflie_drone_app
+# cookbook_cross_prefix := arm-none-eabi-
+# crazyflie_firmware_dir := ../../apps/crazyflie-push-demo/crazyflie-firmware
+#
+# then in smala/:
+# make -j crazyflie_drone_app_objs
+#
+#
+
+objs_cookbook_app := crazyflie_drone_app.o #fake_main.o
+djnn_libs_cookbook_app := crazyflie
+#smala_libs_cookbook_app := smala
+
+# freertos
+freertos_dir := $(crazyflie_firmware_dir)/vendor/FreeRTOS
+freertos_config_dir := $(crazyflie_firmware_dir)/src/config
+freertos_layers_dir := $(crazyflie_firmware_dir)/src
+
+CFLAGS += -I$(freertos_dir)/include \
+	-I$(freertos_dir)/portable/GCC/ARM_CM4F \
+	-I$(freertos_config_dir) \
+	-I$(freertos_layers_dir)/drivers/interface \
+	-I$(freertos_layers_dir)/hal/interface \
+	-I$(freertos_layers_dir)/utils/interface \
+	-I$(freertos_layers_dir)/modules/interface
+
+#freetos-cxx11
+CXXFLAGS += -include $(djnn_path)/src/exec_env/freertos/ext/freertos-cxx11/freertos-cxx11-macros.h
+CXXFLAGS += -I$(djnn_path)/src/exec_env/freertos/ext/freertos-cxx11
+
+# crazyflie
+CFLAGS += -DSTM32F40_41xxx
+CFLAGS += -I$(crazyflie_firmware_dir)/src/lib/CMSIS/STM32F4xx/Include
+CFLAGS += -I$(crazyflie_firmware_dir)/vendor/CMSIS/CMSIS/Include/
+CFLAGS += -mfp16-format=ieee -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16
+#CFLAGS += -pie
+CFLAGS += -fexceptions
+CFLAGS += -ffunction-sections -fdata-sections
+CFLAGS += -fno-math-errno -fno-strict-aliasing -Wdouble-promotion
+#CFLAGS += -fpic
+
+#boost
+CXXFLAGS += -I/usr/local/include
+
+# djnn
+CFLAGS += -DDJNN_CRAZYFLIE
+CFLAGS += -DDJNN_NO_DEBUG
+CFLAGS += -DDJNN_NO_SERIALIZE
+CFLAGS += -DRMT_ENABLED=0
+#CXXFLAGS += $(CFLAGS)
+#CXXFLAGS += -DDJNN_NO_DYNAMIC_CAST
+CXXFLAGS += -DDJNN_USE_FREERTOS
+#CXXFLAGS += -DDJNN_USE_FREERTOS_MAINLOOP
+CXXFLAGS += -DDJNN_USE_STD_THREAD=1
+CXXFLAGS += --rtti #--rtti_data
+CXXFLAGS += -Wno-psabi #https://stackoverflow.com/a/48149400
---
+++ cookbook/extra/crazyflie_drone_app/crazyflie_drone_app.sma
@@ -0,0 +1,69 @@
+use core
+use exec_env
+use base
+
+_native_code_
+%{
+extern "C" {
+#include "commander.h"
+#include "debug.h"
+}
+
+static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, float yawrate)
+{
+  setpoint->mode.z = modeAbs;
+  setpoint->position.z = z;
+
+  setpoint->mode.yaw = modeVelocity;
+  setpoint->attitudeRate.yaw = yawrate;
+
+  setpoint->mode.x = modeVelocity;
+  setpoint->mode.y = modeVelocity;
+  setpoint->velocity.x = vx;
+  setpoint->velocity.y = vy;
+
+  setpoint->velocity_body = true;
+}
+
+setpoint_t setpoint;
+
+void
+hover(CoreProcess* p)
+{
+    float velFront=0, velSide=0, height=0.2;
+    setHoverSetpoint(&setpoint, velFront, velSide, height, 0);
+    commanderSetSetpoint(&setpoint, 3);
+    //DEBUG_PRINT("hover\n");
+}
+
+void
+stop_hover(CoreProcess* p)
+{
+    float velFront=0, velSide=0, height=0;
+    setHoverSetpoint(&setpoint, velFront, velSide, height, 0);
+    commanderSetSetpoint(&setpoint, 3);
+    //DEBUG_PRINT("hover\n");
+}
+
+%}
+
+_define_
+crazyflie_drone_app()
+{
+    NativeAction hover_na (hover, this, 1)
+    NativeAction stop_hover_na (hover, this, 1)
+    FSM truc {
+        State idle {
+            Timer t(3000)
+            |->stop_hover_na
+        }
+        State hover {
+            Timer t(3000)
+            |->hover_na
+        }
+        idle->hover (idle.t.end)
+        hover->idle (hover.t.end)
+    }
+    TextPrinter tp
+    truc.state =:> tp.input
+}

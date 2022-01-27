f9beaf03a5180385c81369dd395c793d23e75b5f
Nicolas VIOT <nicolas.viot@enac.fr>
2022-01-27 10:29:24 +0100

    Add opencv path in video cookbook for linux users

--- cookbook/gui/graphics/video/cookbook_app.mk
+++ cookbook/gui/graphics/video/cookbook_app.mk
@@ -13,5 +13,15 @@
 objs_cookbook_app := main.o
 djnn_libs_cookbook_app := gui display base exec_env core
 #pkg_cookbook_app := opencv
+
+ifneq ($(os),Linux)
 cflags_cookbook_app := $(shell env PKG_CONFIG_PATH=/opt/homebrew/opt/opencv\@3/lib/pkgconfig pkg-config --cflags opencv)
 libs_cookbook_app := $(shell env PKG_CONFIG_PATH=/opt/homebrew/opt/opencv\@3/lib/pkgconfig pkg-config --libs opencv)
+endif
+
+#install opencv with : sudo apt install libopencv-dev
+ifeq ($(os),Linux)
+cflags_cookbook_app := $(shell env PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --cflags opencv4)
+libs_cookbook_app := $(shell env PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --libs opencv4)
+endif
+

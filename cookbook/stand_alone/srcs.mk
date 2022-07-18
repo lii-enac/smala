317fed05ac652bd3e90bf680e9c122e561972472
Mathieu Poirier <mathieu.poirier@enac.fr>
2022-07-18 15:58:22 +0200

    stand_alone - scrs.mk improved pkgdeps

--- cookbook/stand_alone/srcs.mk
+++ cookbook/stand_alone/srcs.mk
@@ -14,9 +14,24 @@ srcs_other := src/foo.cpp
 # or
 #srcs_other := $(shell find $(src_dir) -name "*.cpp")
 
+# ----- PKGS DEPENDENCIES OR EXTERNAL -----
+
 # external libraries
 # CXXFLAGS += $(shell pkg-config --cflags foo)
 # LDFLAGS +=
 # LIBS += $(shell pkg-config --libs foo)
-# or
-# pkgdeps += foo
+# 
+# OR
+# 
+# pkgdeps depending on OS
+# ifeq ($(os),Linux)
+# pkgdeps := #name of the pkgs on apt linux
+# endif
+
+# ifeq ($(os),Darwin)
+# pkgdeps := #name of the pkgs on brew osx
+# endif
+
+# ifeq ($(os),MinGW)
+# pkgdeps := #name of the pkgs on mingw pacman : eq : mingw-w64-x86_64-xxxx
+# endif

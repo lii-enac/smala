bcc995c03b684f2c832f19099d8522f8bb66dda3
U-MAQUETTE-IHM\Root <MAQUETTE-IHM+Root@MAQUETTE-IHM.eleve.interne.enac>
2023-05-24 16:43:47 +0200

    stand_alone - fixed

--- cookbook/stand_alone/src/precompiled.hpp
+++ cookbook/stand_alone/src/precompiled.hpp
@@ -1,14 +1,14 @@
 //#include <iostream>
 //#include "core/utils/build/precompiled.hpp"
 
-#include "core/utils/containers.h"
+// #include "core/utils/containers.h"
 #include "core/ontology/process.h"
 
-namespace djnn {
-    class CoreProcess;
-    class Coupling;
-}
+// namespace djnn {
+//     class CoreProcess;
+//     class Coupling;
+// }
 
-template class djnnstl::basic_string<char>;
-template class djnn::map<djnn::string, djnn::CoreProcess*>;
-template class djnn::vector<djnn::Coupling*>;
+// template class djnnstl::basic_string<char>;
+// template class djnn::map<djnn::string, djnn::CoreProcess*>;
+// template class djnn::vector<djnn::Coupling*>;

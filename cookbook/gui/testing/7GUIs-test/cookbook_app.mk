78bca5e4f46ef134f6790f106fcce7ebfc13c5bb
Mathieu Poirier <mathieu.poirier@enac.fr>
2026-06-18 17:43:09 +0200

    cookbook: fix cppautogui object path in 7GUIs test

--- cookbook/gui/testing/7GUIs-test/cookbook_app.mk
+++ cookbook/gui/testing/7GUIs-test/cookbook_app.mk
@@ -14,17 +14,17 @@
 #
 
 
-cppflags_cookbook_app += -Icppautogui/include/
-
 objs_cookbook_app := Counter_7GUIs.o TempConverter_7GUIs.o FlightBooker_7GUIs.o Timer_7GUIs.o \
 	../../7GUIs/5-CRUD/PersonModel.o ../../7GUIs/5-CRUD/PersonView.o CRUD_7GUIs.o \
 	../../7GUIs/6-circle-drawer/action/UndoRedoManager.o ../../7GUIs/6-circle-drawer/action/CreateAction.o ../../7GUIs/6-circle-drawer/action/ChangeRadiusAction.o  ../../7GUIs/6-circle-drawer/CircleItem.o CircleDrawer_7GUIs.o \
 	../../7GUIs/7-cells/spreadsheet/Spreadsheet.o Cells_7GUIs.o \
-	../../../../../../cppautogui/src/cppautogui_macos.o \
+	../../../../cppautogui/src/cppautogui_macos.o \
 	main.o
 
 djnn_libs_cookbook_app := gui display base exec_env core
 
 smala_libs_cookbook_app := smala
 
-libs_cookbook_app := -framework ApplicationServices -framework CoreFoundation
+#cppautogui
+cppflags_cookbook_app += -Icppautogui/include/
+libs_cookbook_app := -framework ApplicationServices -framework CoreFoundation -framework ImageIO
